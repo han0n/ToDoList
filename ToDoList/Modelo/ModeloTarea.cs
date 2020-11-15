e951736d776747924c7d4ee78e75be75293f3c89
han0n <carferme@hotmail.es>
2020-11-15 12:58:18 +0100

    Creado en Modelo: ModeloTarea con la interfaz INotifyPropertyChanged implementada.

---
+++ ToDoList/Modelo/ModeloTarea.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ToDoList.Modelo
+{
+    class ModeloTarea : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] String titulo = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(titulo));
+        }
+
+        private String titulo;
+
+        public String Titulo
+        {
+            get { return titulo; }
+            set 
+            { 
+                titulo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private String comentario;
+
+        
+
+        public String Comentario
+        {
+            get { return comentario; }
+            set 
+            { 
+                comentario = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+}
