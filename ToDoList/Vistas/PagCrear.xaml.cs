5c6d929dde9ca33c4214f6a5d66e3ff5f3cc2e4a
han0n <carferme@hotmail.es>
2020-11-19 02:40:39 +0100

    Añadido: - Botón de edición implementado pero con defecto: al cancelar la edición, si el usuario ha modificado algún campo, este se actualiza en vez de volver al original (POR RESOLVER). - Método Ordenar que ordena la listaTareas y asigna al contexto de la ListView la listaOrdenada (LvTareas.ItemsSource = listaOrdenada;) para el binding. - Sobrecarga de la clase PagCrear para la modificación de un ModeloTarea "bindeado" desde PagTareas.
    Eliminado:
    - Las directivas de using no usadas en las Vistas.
    
    Modificado:
    - Ya listaTareas no es un ObservableCollection si no un List<ModeloTarea>, el motivo es el posible ordenamiento de este.

--- ToDoList/Vistas/PagCrear.xaml.cs
+++ ToDoList/Vistas/PagCrear.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToDoList.Modelo;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,29 +10,52 @@ namespace ToDoList
     public partial class PagCrear : ContentPage
     {
         ModeloTarea obj_tarea = new ModeloTarea();
-        ObservableCollection<ModeloTarea> listaTareas = new ObservableCollection<ModeloTarea>();
-        public PagCrear(ObservableCollection<ModeloTarea> lista_devuelta)
+        List<ModeloTarea> listaTareas = new List<ModeloTarea>();
+        ModeloTarea obj_recibido = new ModeloTarea();
+        
+        public PagCrear(List<ModeloTarea> lista_devuelta)
         {
             InitializeComponent();
-
             btnCancelar.Clicked += BtnCancelar_Clicked;
             btnGuardar.Clicked += BtnGuardar_Clicked;
 
-            //ModeloTarea obj_tarea = new ModeloTarea();
             BindingContext = this.obj_tarea;
             this.listaTareas = lista_devuelta;
         }
 
+        public PagCrear(List<ModeloTarea> lista_recibida, ModeloTarea tarea_recibida)
+        {
+            InitializeComponent();
+            btnCancelar.Clicked += BtnCancelar_Clicked;
+            btnGuardar.Clicked += BtnGuardar_Clicked;
+
+            this.obj_tarea = tarea_recibida; // Porque en el método de Guardar refiere a obj_tarea
+            this.obj_recibido = tarea_recibida; // Para que se guarde por si cancela
+            
+            this.listaTareas = lista_recibida; // Se necesita porque la clase no guarda el ObservableCollection<ModeloTarea> listaTareas
+            
+
+            BindingContext = tarea_recibida; //Parece el mismo, pero los valores de obj_tarea son los de tarea_recibida
+        }
+
         private void BtnGuardar_Clicked(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new PagTareas(obj_tarea));
+            this.listaTareas.Remove(this.obj_recibido);
             listaTareas.Add(obj_tarea);
             Navigation.PushModalAsync(new PagTareas(listaTareas));
         }
 
         private void BtnCancelar_Clicked(object sender, EventArgs e)
         {
+            //this.listaTareas.Remove(this.obj_tarea);
+            //listaTareas.Add(obj_recibido);
             this.Navigation.PopModalAsync();
+            // Si ha recibido algún objeto (si se han pasado dos parametros a PagCrear en vez de uno)
+            /*if (this.obj_recibido != null)
+            {
+                this.listaTareas.Add(obj_recibido); // Se añade de nuevo a listaTareas
+            }*/
+
         }
     }
 }
--- ToDoList/Vistas/PagTareas.xaml.cs
+++ ToDoList/Vistas/PagTareas.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToDoList.Modelo;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,16 +10,18 @@ namespace ToDoList
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PagTareas : ContentPage
     {
-        ObservableCollection<ModeloTarea> listaTareas = new ObservableCollection<ModeloTarea>();
+        // Cambiado a List para que pueda ordenar y al borrar borre
+        List<ModeloTarea> listaTareas = new List<ModeloTarea>();
         
-        public PagTareas(ObservableCollection<ModeloTarea> lista_pasada)
+        public PagTareas(List<ModeloTarea> lista_pasada)
         {
             InitializeComponent();
             MuestraFecha();
 
             this.listaTareas = lista_pasada;
             //this.listaTareas.Add(obj_binding);
-            LvTareas.ItemsSource = this.listaTareas;
+            //LvTareas.ItemsSource = this.listaTareas;
+            Ordenar();
 
             btnCrear.Clicked += BtnCrear_Clicked;
         }
@@ -37,7 +36,8 @@ namespace ToDoList
                 Titulo = "Ejemplo ", 
                 Comentario = "Esto es un ejemplo de tarea" 
             });
-            LvTareas.ItemsSource = this.listaTareas;
+            //LvTareas.ItemsSource = this.listaTareas;
+            Ordenar();
         }
 
         private void BtnCrear_Clicked(object sender, EventArgs e)
@@ -55,11 +55,39 @@ namespace ToDoList
             lblDiaHoy.Text += dia.ToString() + ", " + fechaDia + " de " + mes;
         }
 
-        private void BtnBorrar_Clicked(object sender, EventArgs e)
+        async private void BtnBorrar_Clicked(object sender, EventArgs e)
         {
             var boton = sender as Button;
             var tarea = boton.BindingContext as ModeloTarea;
-            listaTareas.Remove(tarea);
+
+            bool respuesta = await DisplayAlert("Atención:", 
+                "¿Seguro que quiere borrar la tarea?", "Sí", "No");
+
+            if (respuesta == true)
+            {
+                listaTareas.Remove(tarea);
+                Ordenar();
+            }
+        }
+
+        private void BtnEditar_Clicked(object sender, EventArgs e)
+        {
+            var boton = sender as Button;
+            var tarea = boton.BindingContext as ModeloTarea;
+            // listaTareas.Remove(tarea);
+            Navigation.PushModalAsync(new PagCrear(this.listaTareas, tarea));
+        }
+
+        /* Método que ordena listaTareas y asigna la 
+        List<ModeloTarea> listaOrdenada como ItemsSource de LvTareas */
+        public void Ordenar()
+        {
+            //Ordena las Tareas según su Titulo
+            var listaOrdenada = listaTareas.OrderBy(x => x.Titulo)
+                                  .ToList();
+
+            // Se asigna la nueva fuente de elementos listados
+            LvTareas.ItemsSource = listaOrdenada;
         }
     }
 }
